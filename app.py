71125c01788124223b349413938e5fca4d808b5f
Mohammed Adel Alhydry <50909741+alhydrym7@users.noreply.github.com>
2023-07-25 18:41:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,70 @@
-
 import streamlit as st
 from PIL import Image
 import pandas as pd
-import pickle
 import requests
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier
+from nltk.corpus import stopwords
+import re
+from nltk.stem.porter import PorterStemmer
+import nltk
+nltk.download('stopwords')
+
+# Load the CountVectorizer model
+cv_transforms = pickle.load(open(r'C:\Users\asus\Desktop\Artificial intelligence\Third Year\Summer\EVC\Dataanlysez\cv-transforms.pkl', 'rb'))
+
+# Load the Multinomial Naive Bayes model
+nb_classifier = pickle.load(open(r'C:\Users\asus\Desktop\Artificial intelligence\Third Year\Summer\EVC\Dataanlysez\movie-genre-mnb-model.pkl', 'rb'))
+
+# Load the Random Forest model
+rf_classifier = pickle.load(open(r'C:\Users\asus\Desktop\Artificial intelligence\Third Year\Summer\EVC\Dataanlysez\movie-genre-rfc-model.pkl', 'rb'))
+
+data = pd.read_csv(r"C:\Users\asus\Downloads\movie_recommender_system-main\movie_recommender_system-main\dataset.csv")
+unique_titles = data['genre'].drop_duplicates()
+genres_for_maping = []
+
+for title in unique_titles:
+    genres_for_maping.append(title)
+
+from sklearn.preprocessing import LabelEncoder
+
+label_encoder = LabelEncoder()
+genres_numerical = label_encoder.fit_transform(genres_for_maping)
+genre_mapping_dict = {}
+
+for genre, numerical_value in zip(genres_for_maping, genres_numerical):
+    genre_mapping_dict[genre] = numerical_value
+
+inverted_genre_mapping_dict = {v: k for k, v in genre_mapping_dict.items()}
+genre_mapping_dict = inverted_genre_mapping_dict
+
+def preprocess_text(dialog):
+    ps = PorterStemmer()
+    dialog = re.sub(pattern='[^a-zA-Z]', repl=' ', string=dialog)
+    dialog = dialog.lower()
+    words = dialog.split()
+    dialog_words = [word for word in words if word not in set(stopwords.words('english'))]
+    words = [ps.stem(word) for word in dialog_words]
+    dialog = ' '.join(words)
+    return dialog
+
+def predict_genre_nb(dialog):
+    preprocessed_dialog = preprocess_text(dialog)
+    vectorized_dialog = cv_transforms.transform([preprocessed_dialog])
+    genre_prediction_nb = nb_classifier.predict(vectorized_dialog)[0]
+    genre_name_nb = genre_mapping_dict[genre_prediction_nb]
+
+    return genre_name_nb
+
+def predict_genre_rf(dialog):
+    preprocessed_dialog = preprocess_text(dialog)
+    vectorized_dialog = cv_transforms.transform([preprocessed_dialog])
+    genre_prediction_rf = rf_classifier.predict(vectorized_dialog)[0]
+    genre_name_rf = genre_mapping_dict[genre_prediction_rf]
+
+    return genre_name_rf
 
 image2 = Image.open(r"C:\Users\asus\Downloads\logo.ico")
 st.sidebar.image(image2)
@@ -28,11 +89,10 @@ def recommend(movie, df):
 
     return recommended_movie_names, recommended_movie_posters
 
-
 st.sidebar.info('مرحبًا بك في نظام توصية الأفلام.')
 st.sidebar.info("تم إنشاء وتصميمه بواسطة فريق K-Digital")
 
-page = st.sidebar.selectbox("كيف ترغب في البحث؟", ["الصفحة الرئيسية", "عن طريق التشابه", "عن طريق المعلومات"])
+page = st.sidebar.selectbox("كيف ترغب في البحث؟", ["الصفحة الرئيسية", "عن طريق التشابه", "عن طريق المعلومات","عن طريق وصف للفيلم"])
 
 if page == "الصفحة الرئيسية":
     st.header('مرحبًا بك في نظام توصية الأفلام')
@@ -42,13 +102,13 @@ if page == "الصفحة الرئيسية":
     st.write("<span style='font-family: Arial; color: #94E6E6; font-size: 25px;'>3. شهد محمد </span>", unsafe_allow_html=True)
     st.write("<span style='font-family: Arial; color: #94E6E6; font-size: 25px;'>4. إكرام فراس </span>", unsafe_allow_html=True)
     st.write("<span style='font-family: Arial; color: #C4CA02; font-size: 35px;'>هذا المشروع تحت إشراف:</span>", unsafe_allow_html=True)
-    st.write("<span style='text-align: center: Arial; color: #94E6E6; font-size: 45px;'>الأستاذة مروى </span>", unsafe_allow_html=True)
+    st.write("<span style='text-align: center: Arial; color: #94E6E6; font-size: 20px;'>1. المهندسه مروى </span>", unsafe_allow_html=True)
+    st.write("<span style='text-align: center: Arial; color: #94E6E6; font-size: 20px;'>2. المهندس إبراهيم</span>", unsafe_allow_html=True)
 
 if page == "عن طريق التشابه":
     st.header('نظام توصية الأفلام')
     movies = pickle.load(open('movie_list.pkl', 'rb'))
     similarity = pickle.load(open('similarity.pkl', 'rb'))
-    
 
     movie_list = movies['title'].values
     selected_movie = st.selectbox("اختر الفيلم من القائمة", movie_list)
@@ -60,37 +120,33 @@ if page == "عن طريق التشابه":
             st.text(recommended_movie_names[0])
             st.image(recommended_movie_posters[0], use_column_width=True)
 
-
         with col2:
             st.text(recommended_movie_names[1])
             st.image(recommended_movie_posters[1], use_column_width=True)
-
 
         with col3:
             st.text(recommended_movie_names[2])
             st.image(recommended_movie_posters[2], use_column_width=True)
 
-
-if page == "عن طريق المعلومات":
+elif page == "عن طريق المعلومات":
     st.header("معلومات الفيلم")
 
     program = pd.read_pickle("program.pkl")
 
-    quality = st.text_input("أدخل جودة الفيلم (SD أو HD):").upper()
-    genre = st.text_input("أدخل التصنيف المطلوب:")
-    film_or_series = st.text_input("هل تبحث عن فلم أم مسلسل (S أو M):").upper()
-    num = st.text_input("ادخل عدد التوصيات التي تريدها:")
-
-   
-    if st.button("البحث"):
+    quality_options = ["SD", "HD"]
+    quality = st.selectbox("أدخل جودة الفيلم:", quality_options)
     
-        if num:
-            num = int(num)
-        else:
-            num = 5  
+    genre2 = program["genre"].unique()
+    genre = st.selectbox("أدخل التصنيف المطلوب:", genre2)
 
+    film_or_series_options = ["فلم", "مسلسل"]
+    film_or_series = st.selectbox("هل تبحث عن فلم أم مسلسل:", film_or_series_options)
+
+    num = st.slider("اختر عدد التوصيات التي تريدها:", min_value=1, max_value=10, step=1, value=5)
+    
+    if st.button("البحث"):
         quality_value = 1 if quality == "HD" else 0
-        film_or_series = "movie" if film_or_series == "F" else "series"
+        film_or_series = "movie" if film_or_series == "فلم" else "series"
         filtered_data = program[
             (program["genre"] == genre) &
             (program["type"] == film_or_series) &
@@ -98,9 +154,25 @@ if page == "عن طريق المعلومات":
         ]
         filtered_data = filtered_data.sort_values(by='number_of_watches', ascending=False)
         filtered_data = filtered_data[["type", "genre", "name", "number_of_watches"]]
-        st.dataframe(filtered_data.head(num))
+
+        # تطبيق التنسيق على DataFrame باستخدام applymap
+        styled_data = filtered_data.head(num).style.set_table_attributes('style="text-align: center"')
+
+        # عرض الجدول المُنسق
+        st.table(styled_data)
 
 
+elif page == "عن طريق وصف للفيلم":
+    st.header("وصف الفلم")
+    video_description = st.text_area("اكتب وصفًا للفيلم هنا:")
+    if st.button("التنبؤ بالتصنيف"):
+        predicted_genre_nb = predict_genre_nb(video_description)
+        predicted_genre_rf = predict_genre_rf(video_description)
+
+        # st.write(f"التصنيف المتوقع باستخدام نموذج Naive Bayes: {predicted_genre_nb}")
+        st.write(f" Random Forest التصنيف المتوقع باستخدام:=======>>     {predicted_genre_rf}")
+
+# Add CSS styles to ensure right-to-left text alignment
 st.markdown(
     """
     <style>
@@ -112,9 +184,3 @@ st.markdown(
     """,
     unsafe_allow_html=True,
 )
-
-
-
-
-
-
